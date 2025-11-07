776ea85c8809e4593fc7ee8845eb45ab6e6ff1cf
Gilson Cesar Carlos Filho <113089882+TheFireMonster@users.noreply.github.com>
2025-11-07 00:09:38 -0300

    frontend fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,10 +24,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.js.symbols": "3c01ec03b5de6d62c34e17014d1decd3",
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "dd8c70272f02d76fc7b48c0a2f0276e6",
+"flutter_bootstrap.js": "1b7ba1d98d612ad2686bff9965362da7",
 "index.html": "dca9ec08ca542bd9be23b16b1afb9ef4",
 "/": "dca9ec08ca542bd9be23b16b1afb9ef4",
-"main.dart.js": "806eee38398f7a71ccbab18dc266e0cc",
+"main.dart.js": "35d5e6cbc709c0f6e3784ef52dff9fd6",
 "version.json": "b08cc09f616eac0a3354c34c4af25aaa"};
 // The application shell files that are downloaded before a service worker can
 // start.
